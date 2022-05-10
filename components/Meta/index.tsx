8bdee2ea01e7f7edf9be3f600cfd6b89033d29a5
Josman Proudinat <jmps2812@gmail.com>
2022-05-10 10:22:32 -0600

    Initial layout

---
+++ components/Meta/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Meta() {
+  return (
+    <Head>
+        <title>Escribas</title>
+        <meta name='description' content='Escribas' />
+    </Head>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from "~/components/Layout";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
-
-export default IndexPage
+export default function Home() {
+  return (
+    <Layout>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </Layout>
+  );
+}
